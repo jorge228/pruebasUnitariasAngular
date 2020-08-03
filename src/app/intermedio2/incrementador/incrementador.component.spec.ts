bfe5da4c23a15e11bb77894be0b601ac51d5ca93
jorge228 <jorgelozano228@gmail.com>
2020-08-03 17:15:04 +0200

    spec con el html

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 
 describe('Incremendator Component', () => {
@@ -19,9 +20,15 @@ describe('Incremendator Component', () => {
 
     });
 
-    // it('', () => {
 
+    it('Debe de mostrar la leyenda en el <h3>', () => {
 
-    // });
+        component.leyenda = 'Progreso de carga';
+        // dispara detección de cambios para que se modifica la leyenda
+        fixture.detectChanges(); 
+        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+        expect(elem.innerHTML).toContain('Progreso de carga');
+
+    });
 
 });
