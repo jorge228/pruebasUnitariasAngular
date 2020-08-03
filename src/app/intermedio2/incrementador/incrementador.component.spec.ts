6180d16bb8b37467fb2e4d52e42c99b96bcf4c0f
jorge228 <jorgelozano228@gmail.com>
2020-08-03 19:37:49 +0200

    test cambios html tras event

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -54,4 +54,15 @@ describe('Incremendator Component', () => {
         expect(component.progreso).toBe(50);
     });
 
+    it('El título del componente debe mostrar el progreso', () => {
+        const botones = fixture.debugElement.queryAll(By.css('.btn-primary'));
+        botones[0].triggerEventHandler('click',null);
+        fixture.detectChanges();
+        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+        expect(elem.innerHTML).toContain('45');
+        // expect(component.progreso).toBe(45);
+        // botones[1].triggerEventHandler('click',null);
+        // expect(component.progreso).toBe(50);
+    });
+
 });
