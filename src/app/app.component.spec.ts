8d2560cc0f1b89ef36d84bc7096531bb35bcdb65
jorge228 <jorgelozano228@gmail.com>
2020-08-05 18:47:57 +0200

    test existe ruta a página

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -33,4 +33,25 @@ describe('AppComponent', () => {
     const debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
     expect(debugElement).not.toBeNull();
   });
+
+  it('Debe tener un enlace a la página de /medicos', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const elementos = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    // console.log(elementos);
+
+    let existe = false;
+
+    for (const elem of elementos) {
+      // console.log(elem.attributes.routerLink);
+      // if (elem.attributes.routerLink === '/medicos') existe = true;
+      if (elem.attributes['routerLink'] === '/medicos') {
+        existe = true;
+        break;
+      }
+
+
+    }
+    expect(existe).toBeTruthy();
+  });
+
 });
